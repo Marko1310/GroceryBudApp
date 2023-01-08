c8298588c1d416b9740179f5d43efc7482f43d63
Marko1310 <marko.cabo13@gmail.com>
2023-01-08 16:31:21 +0100

    Implemented alert and clear list functionality

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+const Alert = ({ type, msg, removeAlert }) => {
+  // delete error message after 3 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
 
-export default Alert
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
